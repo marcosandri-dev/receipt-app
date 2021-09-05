5e9aafc4744f6a74443ff4a4a1c33621be1d487d
Marco Sandri <sandri11_04@live.com>
2021-09-06 01:11:24 +0200

    crying in the night over tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+/* eslint-disable no-undef */
+/* eslint-disable react/react-in-jsx-scope */
+import { render, screen } from '@testing-library/react';
+import Header from './components/Header';
 
-/* test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders Receipt App link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Receipt App/i);
   expect(linkElement).toBeInTheDocument();
-}); */
-
-// No test for the moment!
+});
